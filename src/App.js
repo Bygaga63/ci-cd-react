7155359408a85b8b11eca40618806c1b7ade26ca
Bygaga63 <Bygaga63@yandex.ru>
2020-06-21 15:10:11 +0300

    change test, expect fail

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react
+          Learn flutter
         </a>
       </header>
     </div>
